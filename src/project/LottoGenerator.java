d8b959689de64c880823239156aeb4ac32a5c63f
solheek <sooooolhee@naver.com>
2018-06-16 01:18:00 +0000

    issue 1 fix

--- src/project/LottoGenerator.java
+++ src/project/LottoGenerator.java
@@ -10,7 +10,7 @@ public class LottoGenerator{
         int[] result = new int[6];
         
         for (int i=0 ; i<6 ; i++){
-            result[i] = random.nextInt(45);
+            result[i] = random.nextInt(45) + 1;
         }
         return result;
     }
